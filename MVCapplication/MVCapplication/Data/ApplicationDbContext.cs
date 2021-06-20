bc4b11ce0b87eb8f3855b9b5c9047dab05f9abd5
bhanusrivaishnavi <bbsvaishnavi123@gmail.com>
2021-06-20 12:38:58 +0530

    initial commit

---
+++ MVCapplication/MVCapplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVCapplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCapplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
---
+++ MVCapplication/MVCapplication/Data/Migrations/20210620031430_initial migration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCapplication.Data.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
